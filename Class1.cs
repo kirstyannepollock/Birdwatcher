237ac7daf60ed062b921d94e9ddeabdef4a04b89
kirstyannepollock <kpollock@hotmail.co.uk>
2016-07-08 15:16:20 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Birdwatcher
+{
+    public class Class1
+    {
+    }
+}
